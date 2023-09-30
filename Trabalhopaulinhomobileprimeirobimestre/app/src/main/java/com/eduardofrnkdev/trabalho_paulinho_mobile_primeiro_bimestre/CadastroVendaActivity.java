cc2130f3811800f2c1a774718e916e773acaedcf
eduardo i nakai frasson <109884797+eduardonakaidev@users.noreply.github.com>
2023-09-30 20:23:35 -0300

    fiz a parte de clicar nos botao em vendas

--- Trabalhopaulinhomobileprimeirobimestre/app/src/main/java/com/eduardofrnkdev/trabalho_paulinho_mobile_primeiro_bimestre/CadastroVendaActivity.java
+++ Trabalhopaulinhomobileprimeirobimestre/app/src/main/java/com/eduardofrnkdev/trabalho_paulinho_mobile_primeiro_bimestre/CadastroVendaActivity.java
@@ -3,6 +3,8 @@ package com.eduardofrnkdev.trabalho_paulinho_mobile_primeiro_bimestre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -24,6 +26,7 @@ public class CadastroVendaActivity extends AppCompatActivity {
     private int posicaoSelecionada = 0;
     private RadioButton rbaVista;
     private RadioButton rbaPrazo;
+    private TextView tvErroCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,41 @@ public class CadastroVendaActivity extends AppCompatActivity {
         tvContItensSelecionados = findViewById(R.id.tvContItensSelecionados);
         rbaVista = findViewById(R.id.rbaVista);
         rbaPrazo = findViewById(R.id.rbaPrazo);
+        tvErroCliente = findViewById(R.id.tvErroCliente);
 
+
+        spCliente.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView,
+                                       View view, int posicao, long l) {
+                if(posicao > 0){
+                    posicaoSelecionada = posicao;
+                    tvErroCliente.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        btSalvarVenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                salvarVenda();
+            }
+            
+        });
+        carregarClientes();
+        atualizarListaVendas();
+    }
+
+    private void salvarVenda() {
+    }
+
+    private void atualizarListaVendas() {
+    }
+
+    private void carregarClientes() {
     }
 }
