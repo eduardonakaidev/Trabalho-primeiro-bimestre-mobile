15800df58d0b690dc3b4a66d36417d0f17e96159
eduardo i nakai frasson <109884797+eduardonakaidev@users.noreply.github.com>
2023-09-30 19:40:21 -0300

    fiz os importes do xml para o java da venda

--- Trabalhopaulinhomobileprimeirobimestre/app/src/main/java/com/eduardofrnkdev/trabalho_paulinho_mobile_primeiro_bimestre/CadastroVendaActivity.java
+++ Trabalhopaulinhomobileprimeirobimestre/app/src/main/java/com/eduardofrnkdev/trabalho_paulinho_mobile_primeiro_bimestre/CadastroVendaActivity.java
@@ -3,13 +3,40 @@ package com.eduardofrnkdev.trabalho_paulinho_mobile_primeiro_bimestre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.eduardofrnkdev.trabalho_paulinho_mobile_primeiro_bimestre.modules.Produto;
+
+import java.util.ArrayList;
 
 public class CadastroVendaActivity extends AppCompatActivity {
 
+    private Button btGerarCodigoPedido;
+     private Button btSalvarVenda;
+     private Spinner spCliente;
+     private TextView tvListaProdutosSelecionados;
+     private  TextView tvValorTotal;
+     private TextView tvContItensSelecionados;
+    private ArrayList<Produto> listaProdutos;
+    private int posicaoSelecionada = 0;
+    private RadioButton rbaVista;
+    private RadioButton rbaPrazo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_venda);
+        btGerarCodigoPedido = findViewById(R.id.btGerarCodigoPedido);
+        btSalvarVenda = findViewById(R.id.btSalvarVenda);
+        spCliente = findViewById(R.id.spCliente);
+        tvListaProdutosSelecionados = findViewById(R.id.tvListaProdutosSelecionados);
+        tvValorTotal = findViewById(R.id.tvValorTotal);
+        tvContItensSelecionados = findViewById(R.id.tvContItensSelecionados);
+        rbaVista = findViewById(R.id.rbaVista);
+        rbaPrazo = findViewById(R.id.rbaPrazo);
+
     }
 }
--- Trabalhopaulinhomobileprimeirobimestre/app/src/main/java/com/eduardofrnkdev/trabalho_paulinho_mobile_primeiro_bimestre/modules/Venda.java
+++ Trabalhopaulinhomobileprimeirobimestre/app/src/main/java/com/eduardofrnkdev/trabalho_paulinho_mobile_primeiro_bimestre/modules/Venda.java
@@ -4,10 +4,70 @@ import java.util.ArrayList;
 
 public class Venda {
 
-    private Cliente cliente ;
-    private ArrayList<Produto> produtos;
-    private CondicaoPagamento condicaoPagamento;
+    private String codigoPedido;
+    private ArrayList<Produto>  listaProdutos;
+    private Cliente cliente;
     private double valorTotal;
+    private String formaPagamento;
+    private int numeroParcelas;
+
+    public String getCodigoPedido() {
+        return codigoPedido;
+    }
+
+    public void setCodigoPedido(String codigoPedido) {
+        this.codigoPedido = codigoPedido;
+    }
+
+    public ArrayList<Produto> getListaProdutos() {
+        return listaProdutos;
+    }
+
+    public void setListaProdutos(ArrayList<Produto> listaProdutos) {
+        this.listaProdutos = listaProdutos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public int getNumeroParcelas() {
+        return numeroParcelas;
+    }
+
+    public void setNumeroParcelas(int numeroParcelas) {
+        this.numeroParcelas = numeroParcelas;
+    }
+
+    public Venda(String codigoPedido, ArrayList<Produto> listaProdutos, Cliente cliente, double valorTotal, String formaPagamento, int numeroParcelas) {
+
+        this.codigoPedido = codigoPedido;
+        this.listaProdutos = listaProdutos;
+        this.cliente = cliente;
+        this.valorTotal = valorTotal;
+        this.formaPagamento = formaPagamento;
+        this.numeroParcelas = numeroParcelas ;
+    }
 
     public  Venda(){}
 
